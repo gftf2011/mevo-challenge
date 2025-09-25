ed1045cd00eef45e47498e7bb0d578d4e64bd148
Gabriel Ferrari Tarallo Ferraz <gftf2011@gmail.com>
2025-09-25 16:18:42 -0300

    test: add prescription entity validation test for controlled prescriptions greater than 60 days duration

--- tests/unit/prescription/domain/entities/PrescriptionEntity.spec.ts
+++ tests/unit/prescription/domain/entities/PrescriptionEntity.spec.ts
@@ -109,4 +109,25 @@ describe('PrescriptionEntity - Test Suite', () => {
         expect(notificationHandler.getErrors().length).toBe(1);
         expect(notificationHandler.getErrors()[0]).toEqual(new PrescriptionDomainError(`"prescription.duration" can not be less than 0 such as - '-1'`, '-1', 'duration'));
     });
+
+    it('given invalid props with "controlled" equals "True" and "duration" greater than 60, when calls validate(), then should have error', () => {
+        const notificationHandler = NotificationHandler.createEmpty();
+        const prescription = PrescriptionEntity.create({
+            id: '1',
+            date: '2021-01-01',
+            patient_cpf: cpf.generate(false),
+            doctor_crm: '12345',
+            doctor_uf: 'SP',
+            controlled: 'True',
+            medication: 'Medication 1',
+            dosage: '10mg',
+            frequency: '10mg',
+            duration: '61',
+            notes: 'Notes 1'
+        });
+        prescription.validate(notificationHandler);
+        expect(notificationHandler.hasErrors()).toBe(true);
+        expect(notificationHandler.getErrors().length).toBe(1);
+        expect(notificationHandler.getErrors()[0]).toEqual(new PrescriptionDomainError(`"prescription.duration" can not be greater than 60 when "prescriptions.controlled" is True`, '61', 'duration'));
+    });
 });
