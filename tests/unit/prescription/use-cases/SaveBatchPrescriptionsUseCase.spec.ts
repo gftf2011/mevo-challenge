88d9b41bba5ad86163dc01a6ea3e3e755efab5cd
Gabriel Ferrari Tarallo Ferraz <gftf2011@gmail.com>
2025-09-27 15:28:43 -0300

    test: add SaveBatchPrescriptionsUseCase test scenario for valid prescription list

--- tests/unit/prescription/use-cases/SaveBatchPrescriptionsUseCase.spec.ts
+++ tests/unit/prescription/use-cases/SaveBatchPrescriptionsUseCase.spec.ts
@@ -1,3 +1,5 @@
+import { cpf } from 'cpf-cnpj-validator';
+
 import { SaveBatchPrescriptionsUseCase } from "../../../../src/prescription/use-cases/SaveBatchPrescriptionsUseCase";
 import { InMemoryPrescriptionRepository } from "../../../../src/prescription/infra/repositories/InMemoryPrescriptionRepository";
 
@@ -14,6 +16,27 @@ describe("SaveBatchPrescriptionsUseCase - Test Suite", () => {
         expect(result).toBeDefined();
         expect(result.valid_records).toBe(0);
         expect(result.processed_records).toBe(0);
+        expect(result.errors.length).toBe(0);
+    });
+
+    it("given input with valid prescriptions list, when calls execute(), then should return valid processed records", async () => {
+        const sut = new SaveBatchPrescriptionsUseCase(repository);
+        const result = await sut.execute({ prescriptions: [{
+            id: "1",
+            date: "2021-01-01",
+            patient_cpf: cpf.generate(false),
+            doctor_crm: "12345",
+            doctor_uf: "SP",
+            controlled: "True",
+            medication: "Medication",
+            dosage: "10mg",
+            frequency: "10mg",
+            duration: "10mg",
+            notes: "Notes",
+        }], current_line: 1 });
+        expect(result).toBeDefined();
+        expect(result.valid_records).toBe(1);
+        expect(result.processed_records).toBe(1);
         expect(result.errors.length).toBe(0);
     });
 
